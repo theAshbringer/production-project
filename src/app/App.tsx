e2b2c25bf9fb92cf57d5adde183927cfe1dbdb76
theAsbringer <kristya1310@gmail.com>
2022-09-20 00:08:32 +0700

    feat: move pages acc. to FSD and setup public API

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,10 @@ import { Suspense } from "react"
 import { Link } from "react-router-dom"
 import { Route, Routes } from "react-router-dom"
 import './styles/index.scss'
-import { AboutPageAsync } from "pages/AboutPage/AboutPage.acync"
-import { MainPageAsync } from "pages/MainPage/MainPage.async"
 import { useTheme } from "app/providers/ThemeProvider"
 import { classNames } from "helpers/classNames/classNames"
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -17,8 +17,8 @@ const App = () => {
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={'/about'} element={<AboutPageAsync />} />
-          <Route path={'/'} element={<MainPageAsync />} />
+          <Route path={'/about'} element={<AboutPage />} />
+          <Route path={'/'} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
