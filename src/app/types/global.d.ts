122e085b7dd62ddbef351774aa2e4c620fb3a160
theAsbringer <kristya1310@gmail.com>
2022-09-23 22:11:08 +0700

    fix: fix ts declaration for image types

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,6 +1,11 @@
 declare module "*.module.scss";
 
-declare module "*.svg" {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default content;
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg' {
+  import React from 'react';
+
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
 }
