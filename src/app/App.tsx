da0aeed3158f4ceb87dca63ffb63fba77bf297e8
theAsbringer <kristya1310@gmail.com>
2022-09-24 16:29:46 +0700

    test: change locale on button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,36 @@ import { classNames } from "shared/lib/classNames/classNames"
 import { AppRouter } from "./providers/router"
 import { Navbar } from "widgets/Navbar"
 import { Sidebar } from 'widgets/Sidebar'
+import { Suspense } from 'react'
+import { useTranslation } from 'react-i18next'
+
+const Component = () => {
+  const [t, i18n] = useTranslation();
+
+  const toggle = () => {
+    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+  }
+
+  return (
+    <div>
+      <button onClick={toggle}>{t('Перевести')}</button>
+      <div>{t('Тестовый перевод')}</div>
+    </div>
+  )
+}
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Navbar />
-      <div className='content-page'>
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback='Loading translation'>
+        <Component />
+        <Navbar />
+        <div className='content-page'>
+          <Sidebar />
+          <AppRouter />
+        </div></Suspense>
     </div>
   )
 }
