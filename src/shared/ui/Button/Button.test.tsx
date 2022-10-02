3013af1200418401440e0a2329cb94af8d9586de
theAsbringer <kristya1310@gmail.com>
2022-10-02 21:11:44 +0700

    test: more button tests

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import { Button } from './Button';
+import { Button, ThemeButton } from './Button';
 
 describe('test button', () => {
-  test('test', () => {
+  test('render button', () => {
     render(<Button>TEST</Button>);
     expect(screen.getByText('TEST')).toBeInTheDocument();
+  });
+
+  test('render button with theme', () => {
+    render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+    expect(screen.getByText('TEST')).toHaveClass('clear');
   });
 });
