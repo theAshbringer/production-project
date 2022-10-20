fe2b6149cd8dddb42bace52de9656193b38c081e
theAsbringer <kristya1310@gmail.com>
2022-10-20 19:41:49 +0700

    feat: add modal to navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,20 +8,11 @@ import { AppRouter } from './providers/router';
 
 const App = () => {
   const { theme } = useTheme();
-  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback="Loading">
         <Navbar />
-        <button onClick={() => setIsOpen(true)}>Открыть модалку</button>
-        <Modal
-          isOpen={isOpen}
-          onClose={() => setIsOpen(false)}
-        // eslint-disable-next-line i18next/no-literal-string
-        >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis vel iste, eaque omnis aliquam minima voluptas velit accusantium repudiandae sit perferendis quis consectetur. Nihil pariatur distinctio nemo alias vitae qui!
-        </Modal>
         <div className="content-page">
           <Sidebar />
           <AppRouter />
--- src/widgets/Navbar/Navbar.tsx
+++ src/widgets/Navbar/Navbar.tsx
@@ -1,5 +1,8 @@
+import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib';
+import { Button, ButtonTheme } from 'shared/ui/Button';
+import { Modal } from 'shared/ui/Modal';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -8,10 +11,28 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsLoginOpen((prev) => !prev);
+  }, []);
 
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
-      <div className={cls.links} />
+      <Button
+        onClick={onToggleModal}
+        theme={ButtonTheme.BACKGROUND_INVERTED}
+        className={cls.links}
+      >
+        {t('login')}
+      </Button>
+      <Modal
+        onClose={onToggleModal}
+        isOpen={isLoginOpen}
+        // eslint-disable-next-line i18next/no-literal-string
+      >
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis vel iste, eaque omnis aliquam minima voluptas velit accusantium repudiandae sit perferendis quis consectetur. Nihil pariatur distinctio nemo alias vitae qui!
+      </Modal>
     </div>
   );
 };
