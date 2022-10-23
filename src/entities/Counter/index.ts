f1172616cfb7e080ad3567e773aace7ebf4aebf4
theAsbringer <kristya1310@gmail.com>
2022-10-23 19:27:14 +0700

    feat: add counter for testing redux

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,1 +1,3 @@
+export { Counter } from './ui/Counter';
+export { counterSlice, counterReducer } from './model/slice/counterSlice';
 export type { CounterSchema } from './model/types/counterSchema';
