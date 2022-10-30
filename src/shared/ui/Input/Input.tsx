7f20969eb2f219f17516d269ba395a1004b33549
Kristina Jumanova <kristya1310@gmail.com>
2022-10-30 20:24:28 +0700

    style: shell-like input with fixed caret

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -9,10 +9,15 @@ interface InputProps extends HTMLInputProps {
   value?: string;
   onChange?: (value: string) => void;
   type?: string;
+  placeholder?: string;
 }
 export const Input = memo((props: InputProps) => {
   const {
-    className, value, onChange, type = 'text',
+    className,
+    value,
+    onChange,
+    placeholder,
+    type = 'text',
   } = props;
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -20,11 +25,23 @@ export const Input = memo((props: InputProps) => {
   };
 
   return (
-    <input
-      className={classNames(cls.input, {}, [className])}
-      value={value}
-      onChange={onChangeHandler}
-      type={type}
-    />
+    <div
+      className={classNames(cls.inputWrapper, {}, [className])}
+    >
+      {placeholder && (
+      <div className={cls.placeholder}>
+        {`${placeholder}>`}
+      </div>
+      )}
+      <div className={cls.caretWrapper}>
+        <input
+          className={cls.input}
+          value={value}
+          onChange={onChangeHandler}
+          type={type}
+        />
+        <span className={cls.caret} />
+      </div>
+    </div>
   );
 });
