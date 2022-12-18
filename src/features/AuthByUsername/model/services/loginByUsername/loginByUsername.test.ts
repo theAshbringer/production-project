d2076eba4c81f98c8a3d26fe5fc6be4a00efc16f
theAshbringer <kristya1310@gmail.com>
2022-12-18 15:00:26 +0700

    test(loginByUsername): using class for async thunks

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -1,8 +1,8 @@
 import { LoginSchema } from 'features/AuthByUsername';
 import { User } from 'entities/User/model/types/userSchema';
 import axios from 'axios';
-import { Dispatch } from '@reduxjs/toolkit';
 import { userActions } from 'entities/User';
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
 import { loginByUsername } from './loginByUsername';
 
 jest.mock('axios');
@@ -10,26 +10,17 @@ jest.mock('axios');
 const mockedAxios = jest.mocked(axios, true);
 
 describe('test loginByUsername', () => {
-  let dispatch: Dispatch;
-  let getState: () => void;
   const login: LoginSchema = { username: 'login', password: 'password' };
 
-  beforeEach(() => {
-    dispatch = jest.fn();
-    getState = jest.fn();
-  });
-
   test('success login', async () => {
-    const user: User = {
-      id: '1',
-      username: 'login',
-    };
+    const user: User = { id: '1', username: 'login' };
     mockedAxios.post.mockReturnValue(Promise.resolve({ data: user }));
-    const action = loginByUsername(login);
 
-    const result = await action(dispatch, getState, undefined);
-    expect(dispatch).toBeCalledWith(userActions.setAuthData(user));
-    expect(dispatch).toBeCalledTimes(3);
+    const thunk = new TestAsyncThunk(loginByUsername);
+    const result = await thunk.callThunk(login);
+
+    expect(thunk.dispatch).toBeCalledWith(userActions.setAuthData(user));
+    expect(thunk.dispatch).toBeCalledTimes(3);
     expect(mockedAxios.post).toBeCalled();
     expect(result.meta.requestStatus).toBe('fulfilled');
     expect(result.payload).toEqual(user);
@@ -37,10 +28,11 @@ describe('test loginByUsername', () => {
 
   test('error login', async () => {
     mockedAxios.post.mockReturnValue(Promise.resolve({ status: 401 }));
-    const action = loginByUsername(login);
 
-    const result = await action(dispatch, getState, undefined);
-    expect(dispatch).toBeCalledTimes(2);
+    const thunk = new TestAsyncThunk(loginByUsername);
+    const result = await thunk.callThunk(login);
+
+    expect(thunk.dispatch).toBeCalledTimes(2);
     expect(mockedAxios.post).toBeCalled();
     expect(result.meta.requestStatus).toBe('rejected');
     expect(result.payload).toBe('error');
