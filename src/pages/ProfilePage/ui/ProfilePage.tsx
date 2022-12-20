a209841cce794c473a79f6b106a22c59ca241ae5
theAshbringer <kristya1310@gmail.com>
2022-12-20 23:17:27 +0700

    feat: profile entity

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,16 +1,26 @@
+import { profileReducer } from 'entities/Profile';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { DynamicModuleLoader, ReducerList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 
 interface ProfilePageProps {
   className?: string;
 }
+
+const initialReducers: ReducerList = {
+  profile: profileReducer,
+};
+
 const ProfilePage = memo(({ className }: ProfilePageProps) => {
   const { t } = useTranslation('profile');
   return (
-    <div className={classNames('', {}, [className])}>
-      {t('profile-page')}
-    </div>
+    <DynamicModuleLoader reducers={initialReducers} removeAfterUnmount>
+      <div className={classNames('', {}, [className])}>
+        {t('profile-page')}
+      </div>
+
+    </DynamicModuleLoader>
   );
 });
 
